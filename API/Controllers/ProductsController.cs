d0562ab29af2b3c5c1af8f5f1f07785dae2e3e38
Sky <skymsdn@hotmail.com>
2022-07-20 08:41:49 +0800

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "this will be a list of products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(string id)
+        {
+            return "single product";
+        }
+    }
+}
