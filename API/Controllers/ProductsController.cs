cf9689ee2750cebfacf9938c38c0dc5fcf810594
Sky <skymsdn@hotmail.com>
2022-08-10 01:04:45 +0800

    use repository in ProductController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,25 +14,26 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly ILogger<ProductsController> _logger;
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repository;
 
-        public ProductsController(ILogger<ProductsController> logger, StoreContext context)
+        public ProductsController(ILogger<ProductsController> logger, IProductRepository repository)
         {
+            _repository = repository;
             _logger = logger;
-            _context = context;
+
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repository.GetProductsAsync();
             return Ok(products);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(string id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repository.GetProductByIdAsync(id);
         }
     }
 }
